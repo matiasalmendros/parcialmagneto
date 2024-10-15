e4d5ed8711bacb631b94f3b8ebd3c8d9571165d6
Matias Almendros <128422026+matiasalmendros@users.noreply.github.com>
2024-10-15 20:46:01 -0300

    Add files via upload

---
+++ Parcial magneto/src/main/java/org/parcial_1/controllers/DnaController.java
@@ -0,0 +1,32 @@
+package org.parcial_1.controllers;
+
+import org.parcial_1.dto.DnaRequest;
+import org.parcial_1.dto.DnaResponse;
+import org.parcial_1.services.DnaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/mutant")
+public class DnaController {
+
+    private final DnaService dnaService;
+
+    public DnaController(DnaService dnaService) {
+        this.dnaService = dnaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<DnaResponse> checkMutant(@Valid @RequestBody DnaRequest dnaRequest) {
+        boolean isMutant = dnaService.analyzeDna(dnaRequest.getDna());
+        DnaResponse dnaResponse = new DnaResponse(isMutant);
+        if (isMutant) {
+            return ResponseEntity.ok(dnaResponse);
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(dnaResponse);
+        }
+    }
+
+}
