712c94237d747c3db6e7bc1290bb0621f987c722
Matias Almendros <128422026+matiasalmendros@users.noreply.github.com>
2024-10-15 21:01:52 -0300

    Add files via upload

---
+++ src/main/java/org/parcial_1/controllers/StatsController.java
@@ -0,0 +1,25 @@
+package org.parcial_1.controllers;
+
+import org.parcial_1.dto.StatsResponse;
+import org.parcial_1.services.StatsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stats")
+public class StatsController {
+
+    private final StatsService statsService;
+
+    @Autowired
+    public StatsController(StatsService statsService) {
+        this.statsService = statsService;
+    }
+
+    @GetMapping
+    public StatsResponse getStats() {
+        return statsService.getStats();
+    }
+}
